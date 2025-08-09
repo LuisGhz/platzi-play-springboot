0fd3bbf87dae514a65dceb01562193ee293e3e46
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-09 13:25:21 -0600

    Minor change

--- src/main/java/dev/luisghtz/platzi_play/persistence/MovieEntityRepository.java
+++ src/main/java/dev/luisghtz/platzi_play/persistence/MovieEntityRepository.java
@@ -3,7 +3,6 @@ package dev.luisghtz.platzi_play.persistence;
 import dev.luisghtz.platzi_play.domain.dto.MovieDto;
 import dev.luisghtz.platzi_play.domain.repository.MovieRepository;
 import dev.luisghtz.platzi_play.persistence.crud.CrudMovieEntity;
-import dev.luisghtz.platzi_play.persistence.entity.MovieEntity;
 import dev.luisghtz.platzi_play.persistence.mapper.MovieMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
