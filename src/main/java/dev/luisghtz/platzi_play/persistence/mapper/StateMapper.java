769aecfdf115697afc592b35bf98f78a37370f6f
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-09 14:25:48 -0600

    Handle null

--- src/main/java/dev/luisghtz/platzi_play/persistence/mapper/StateMapper.java
+++ src/main/java/dev/luisghtz/platzi_play/persistence/mapper/StateMapper.java
@@ -6,11 +6,15 @@ public class StateMapper {
 
     @Named("stringToBoolean")
     public static Boolean toBoolean(String state) {
+        if (state == null || state.isEmpty())
+            return false;
         return "D".equalsIgnoreCase(state);
     }
 
     @Named("booleanToString")
     public static String toString(Boolean state) {
+        if (state == null)
+            return "D";
         return state ? "D" : "F";
     }
 }
