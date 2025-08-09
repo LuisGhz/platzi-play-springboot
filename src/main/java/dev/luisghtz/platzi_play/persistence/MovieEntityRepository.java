99a170ccaa704fd265c08c6f6412ca3d7027b739
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-09 13:17:04 -0600

    Create movie entity repository

---
+++ src/main/java/dev/luisghtz/platzi_play/persistence/MovieEntityRepository.java
@@ -0,0 +1,25 @@
+package dev.luisghtz.platzi_play.persistence;
+
+import dev.luisghtz.platzi_play.domain.dto.MovieDto;
+import dev.luisghtz.platzi_play.domain.repository.MovieRepository;
+import dev.luisghtz.platzi_play.persistence.crud.CrudMovieEntity;
+import dev.luisghtz.platzi_play.persistence.entity.MovieEntity;
+import dev.luisghtz.platzi_play.persistence.mapper.MovieMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MovieEntityRepository implements MovieRepository {
+    private final CrudMovieEntity crudMovieEntity;
+    private final MovieMapper movieMapper;
+
+    @Override
+    public List<MovieDto> getAll() {
+        return movieMapper.toDtoList(
+                crudMovieEntity.findAll()
+        );
+    }
+}
