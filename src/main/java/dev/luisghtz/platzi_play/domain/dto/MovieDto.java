4adcbc5b80e031da4b816d31e0df0ada4b433252
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-09 13:25:44 -0600

    Change genre type

--- src/main/java/dev/luisghtz/platzi_play/domain/dto/MovieDto.java
+++ src/main/java/dev/luisghtz/platzi_play/domain/dto/MovieDto.java
@@ -1,11 +1,13 @@
 package dev.luisghtz.platzi_play.domain.dto;
 
+import dev.luisghtz.platzi_play.domain.Genre;
+
 import java.time.LocalDate;
 
 public record MovieDto(
         String title,
         Integer duration,
-        String genre,
+        Genre genre,
         LocalDate releaseDate,
         Double rating
 ) {
