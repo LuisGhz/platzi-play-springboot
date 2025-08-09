8a8487689268241134179c68c60d8a89f2adf8e9
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-09 14:06:02 -0600

    Implement response entity

--- src/main/java/dev/luisghtz/platzi_play/web/controllers/MovieController.java
+++ src/main/java/dev/luisghtz/platzi_play/web/controllers/MovieController.java
@@ -3,6 +3,7 @@ package dev.luisghtz.platzi_play.web.controllers;
 import dev.luisghtz.platzi_play.domain.dto.MovieDto;
 import dev.luisghtz.platzi_play.domain.services.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +18,15 @@ public class MovieController {
     private final MovieService movieService;
 
     @GetMapping
-    public List<MovieDto> getAll() {
-        return movieService.getAll();
+    public ResponseEntity<List<MovieDto>> getAll() {
+        return ResponseEntity.ok(movieService.getAll());
     }
 
     @GetMapping("/{id}")
-    public MovieDto getById(@PathVariable Long id) {
-        return movieService.getById(id);
+    public ResponseEntity<MovieDto> getById(@PathVariable Long id) {
+        var entity = movieService.getById(id);
+        return entity != null
+                ? ResponseEntity.ok(entity)
+                : ResponseEntity.notFound().build();
     }
 }
