f24a00b230b9690a62d0a643ae4c049621734ae7
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-09 13:15:44 -0600

    Create movie cotnroller

---
+++ src/main/java/dev/luisghtz/platzi_play/web/controllers/MovieController.java
@@ -0,0 +1,22 @@
+package dev.luisghtz.platzi_play.web.controllers;
+
+import dev.luisghtz.platzi_play.domain.dto.MovieDto;
+import dev.luisghtz.platzi_play.domain.services.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService movieService;
+
+    @GetMapping
+    public List<MovieDto> getAll() {
+        return movieService.getAll();
+    }
+}
