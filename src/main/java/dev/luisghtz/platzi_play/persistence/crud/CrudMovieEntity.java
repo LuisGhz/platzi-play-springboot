6c5f29667c6bd3ef44ecdd7eb5ec19145ecec5a2
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-09 11:52:13 -0600

    Create crud movie entity

---
+++ src/main/java/dev/luisghtz/platzi_play/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,7 @@
+package dev.luisghtz.platzi_play.persistence.crud;
+
+import dev.luisghtz.platzi_play.persistence.entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+}
