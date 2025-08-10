eddaed1b3bb40c3a73979f5012e8a92359f358fa
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-10 13:53:28 -0600

    Create suggest method

--- src/main/java/dev/luisghtz/platzi_play/domain/services/PlatziPlayAIService.java
+++ src/main/java/dev/luisghtz/platzi_play/domain/services/PlatziPlayAIService.java
@@ -1,5 +1,6 @@
 package dev.luisghtz.platzi_play.domain.services;
 
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 
@@ -10,4 +11,11 @@ public interface PlatziPlayAIService {
       Usa menos de 120 caracteres y hazlo con el estilo de Platzi.
       """)
   String generateGreeting();
+
+  @SystemMessage("""
+          Eres un experto en cine que recomienda películas personalizadas según los gustos del usuario.
+          Debes recomendar máximo 3 películas.
+          No incluyas pe´luculas que estén por fuera de la plataforma PlatziPlay.|
+          """)
+  String generateMoviesSuggestions(@UserMessage String userMessage);
 }
