18fe15867c07fcfa762bc089011455da826c0e9a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-08-10 13:38:05 -0600

    Add update method

--- src/main/java/dev/luisghtz/platzi_play/persistence/MovieEntityRepository.java
+++ src/main/java/dev/luisghtz/platzi_play/persistence/MovieEntityRepository.java
@@ -1,12 +1,14 @@
 package dev.luisghtz.platzi_play.persistence;
 
 import dev.luisghtz.platzi_play.domain.dto.MovieDto;
+import dev.luisghtz.platzi_play.domain.dto.UpdateMovieDto;
 import dev.luisghtz.platzi_play.domain.repository.MovieRepository;
 import dev.luisghtz.platzi_play.persistence.crud.CrudMovieEntity;
 import dev.luisghtz.platzi_play.persistence.mapper.MovieMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -34,6 +36,21 @@ public class MovieEntityRepository implements MovieRepository {
         var movieEntity = movieMapper.toEntity(movieDto);
         return movieMapper.toDto(
                 crudMovieEntity.save(movieEntity)
+        );
+    }
+
+    @Override
+    public MovieDto update(Long id, UpdateMovieDto movieDto) {
+        var entity = crudMovieEntity.findById(id).orElse(null);
+
+        if (entity == null) {
+            return null;
+        }
+
+        movieMapper.updateEntityFromDto(movieDto, entity);
+
+        return movieMapper.toDto(
+                crudMovieEntity.save(entity)
         );
     }
 }
